dded657cf3386b6e5ce4e9541e45495ab25b2d06
Toshi-North <tnorth24@vt.edu>
2026-09-03 18:43:23 -0400

    Update Toshi.java

--- Toshi.java
+++ Toshi.java
@@ -1,5 +1,5 @@
 public class Toshi {
     public static void main(String[] args) {
-        System.out.println("Toshi is on the team. hi");
+        System.out.println("Toshi is on the team.");
     }
 }
